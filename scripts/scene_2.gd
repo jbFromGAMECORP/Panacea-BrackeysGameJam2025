186a6ed4e19f0c816269e7ffa63c71e5d68adfe0
JuniperMay <Junipermayjune1@gmail.com>
2025-02-20 21:41:37 -0800

    Created framework for room changes
    Created framework for scene changes, you can test this by going to Test_Scene and moving to either room boundry. You will be teleported to a new scene, however the character will not load in due to no singleton being set up.

---
+++ scripts/scene_2.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var spawn_object = preload("res://scenes/Main Character.tscn")
+@export var scale_factor = Vector2(1.5, 1.5)
+
+
+func _ready() -> void:
+	spawn()
+
+func spawn():
+	var obj = spawn_object.instantiate()
+	obj.position = Vector2(-10,-5)
+	obj.get_node("Animation").scale = scale_factor
+	add_child(obj)
+	
