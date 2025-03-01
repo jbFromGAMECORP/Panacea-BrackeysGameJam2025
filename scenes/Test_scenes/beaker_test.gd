d6359e9b0d247a5590fa4a86843eabd14a3b4204
TorqueMcFearson <160625369+TorqueMcFearson@users.noreply.github.com>
2025-03-01 16:39:17 -0700

    Beaker Color Minigame v0.1
     - Typo and other small fixes

--- scenes/Test_scenes/beaker_test.gd
+++ scenes/Test_scenes/beaker_test.gd
@@ -26,7 +26,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	beaker_color.text = "Beaker color:\n" + str(main_beaker.color)
+	beaker_color.text = "Beaker color:\n" + format_color(main_beaker.color)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -67,7 +67,7 @@ func _on_timer_timeout() -> void:
 	diff += abs(main_beaker.color.b - goal_beaker.color.b)
 	diff += abs(main_beaker.color.g - goal_beaker.color.g)
 	diff += abs(main_beaker.color.r - goal_beaker.color.r)
-	color_difference.text = "Color Difference:\n" +str(diff)
+	color_difference.text = "Color\nDifference:\n" +str(snappedf(diff,.01))
 	if diff < GOAL_DIFFERENCE:
 		$"Success?".text = "✅"
 	else:
@@ -79,7 +79,14 @@ func _on_empty_beaker_pressed() -> void:
 	
 
 func _on_new_color_request_pressed() -> void:
-	var random_colour := Color(randf(),randf(),randf(),).clamp(Color(0, 0, 0, 0),  Color(.60, .60, .60, 1))
+	var random_colour := Color(randf_range(0.3,.8),randf_range(0.3,.8),randf_range(0.3,.8))
 	goal_beaker.color = random_colour
-	goal_color.text = "Goal color:\n" + str(random_colour) # Replace with function body.
+	goal_color.text = "Goal color:\n" + format_color(random_colour) # Replace with function body.
 	_on_timer_timeout()
+
+func format_color(color:Color):
+	var r = "R:" + str(snappedf(color.r,.01))
+	var g = "G:" + str(snappedf(color.g,.01))
+	var b = "B:" + str(snappedf(color.b,.01))
+	return "%s, %s, %s" % [r,g,b]
+	
