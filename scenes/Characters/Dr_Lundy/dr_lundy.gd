ebf04b4a35dfff4049959062f287feea1407e00a
Sari S <sestissi16@earlham.edu>
2025-03-11 00:27:36 -0700

    created test dialogues and a dialogic character for lundy

--- scenes/Characters/Dr_Lundy/dr_lundy.gd
+++ scenes/Characters/Dr_Lundy/dr_lundy.gd
@@ -5,7 +5,7 @@ var is_chatting = false
 
 #Change with dialogic timelines for Lundy
 func _ready():
-	Dialogic.preload_timeline(load("res://Dialogic_Items/Timelines/testGameTip.dtl"))
+	Dialogic.preload_timeline(load("res://Dialogic_Items/Timelines/interactWithLundyTip.dtl"))
 	preload("res://Dialogic_Items/Styles/textBubbleStyle.tres").prepare()
 	randomize()
 	Dialogic.signal_event.connect(DialogicSignal)
@@ -14,7 +14,7 @@ func _process(delta):
 		
 	if player_in_area:
 		if Input.is_action_just_pressed("e"):
-			run_dialogue("testDialogue")
+			run_dialogue("drLundyTestDialogue")
 			
 func run_dialogue(dialogue_string):
 	is_chatting = true
@@ -32,8 +32,8 @@ func _on_chat_detection_body_entered(body):
 	if body.has_method("player"):
 		player_in_area = true
 	var layout = Dialogic.Styles.load_style("textBubbleStyle") # Use the name of the STYLE here
-	layout.register_character(load("res://Dialogic_Items/Characters/randomScientist.dch"), $".")
-	Dialogic.start("testGameTip")
+	layout.register_character(load("res://Dialogic_Items/Characters/drLundy.dch"), $".")
+	Dialogic.start("interactWithLundyTip")
 
 func _on_chat_detection_body_exited(body):
 	if body.has_method("player"):
