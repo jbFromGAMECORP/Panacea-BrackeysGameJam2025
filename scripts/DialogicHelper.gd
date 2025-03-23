8b726e5bb557e0ea20203aef74657afd2a0f219d
TorqueMcFearson <160625369+TorqueMcFearson@users.noreply.github.com>
2025-03-23 00:13:26 -0700

    DH Namebox Sider & Speaker Shake
     - DialogicHelper now ties into speaker changes and make make portraits do stuff automatically when becoming the new speaker. (Highlight, grow, shake)
     - DialogicHelper now determines namebox position based on speaker position.

--- scripts/DialogicHelper.gd
+++ scripts/DialogicHelper.gd
@@ -3,13 +3,11 @@ extends Node
 ## Dialogic helper is for storing things like string names of styles, timelines, and characters. 
 ## So we don't have to load them with strings everywhere. It will also be used for preloading all 
 ## the styles and timelines in a scene upon load.
-
 ## NOTE: I didn't just add this to the Dialogic Singleton incase it ever updates and erases any changes we make to it.
 
-## Inner class named "STYLE", mostly just for grouping all used styles in to const variables for easy access. 
-
 var current_char = null
-# Example: "DH.STYLE.FLOAT_DIALOG"
+
+
 const styles :Array = [
 	preload("res://Dialogic_Items/Styles/GameNarrationCustom1Style/customGameInfoStyle1.tres"),
 	preload("res://Dialogic_Items/Styles/GameNarrationCustom1Style/customGameInfoStyle1.tres"),
@@ -20,26 +18,39 @@ const styles :Array = [
 	preload("res://Dialogic_Items/Styles/testCustomStyle.tres"),
 	preload("res://Dialogic_Items/Styles/textBubbleStyle.tres"),
 	]
+
+@onready var portrait_system = Dialogic.get_subsystem("Portraits")
+@onready var text_system = Dialogic.get_subsystem("Text")
+
+## Inner class named "STYLE", mostly just for grouping all used styles in to const variables for easy access. 
 class STYLE:
 	const FLOAT_DIALOG = "customGameInfoStyle1"
 	const BUBBLE = "testBubbleStyle"
 	const SCIENTIST = "customScientistStyle1"
 	const PATIENT = "customPatientStyle1"
-	
+
+
+# TODO: Make sure all styles are in one folder, so we can just parse the folder instead. see commented code below
 func _ready() -> void:
-	# preloads all styles
-	# TODO: Make sure all styles are in one folder, so we can just parse the folder instead. see commented code below
-	for style in styles:
-		style.prepare()
-		
-	# preload all timelines
+	preload_styles()
+	preload_timelines()
+	connect_signals()
+
+
+func preload_styles():
+	for style in styles: style.prepare()
+
+
+func preload_timelines():
 	for filename in DirAccess.get_files_at("res://Dialogic_Items/Timelines/"):
 		if filename.ends_with("dtl"):
 			Dialogic.preload_timeline(load("res://Dialogic_Items/Timelines/"+filename))
-			
-	#for filename in DirAccess.get_files_at("res://Dialogic_Items/Styles/"):
-		#load("res://Dialogic_Items/Styles/"+filename).prepare()
-	print(Dialogic.get_subsystem("Text").text_started.connect(speaker_changed)) 
+
+ 
+func connect_signals():
+	Dialogic.get_subsystem("Text").text_started.connect(speaker_changed)
+	Dialogic.timeline_ended.connect(func(x=0):print("TIMELINE ENDED"))
+
 
 ## Simple helper that will find the textbox and pass the desired textbox position.
 func set_position(layout,pos):
@@ -48,14 +59,24 @@ func set_position(layout,pos):
 	else: printerr("Dialogic Helper | set_position(): Cannot find node 'VN_TextboxLayer', \
 Make sure you called 'Dialogic.Styles.load_style(DH.STYLE.FLOAT_DIALOG)`.")
 
-func speaker_changed(info):
-	if not current_char: 
-		print("Dialog Started!!")
-	elif info["character"] != current_char:
-		print("SPEAKER CHANGED!!\n",info)
-	else: return
-	current_char = info["character"]
-	var portraits = Dialogic.get_subsystem("Portraits")
-	portraits.animate_character(current_char, "Tada", .2,)
-	#portraits.
-	#animate_character(character: DialogicCharacter, animation_path: String, length: float, repeats := 1, is_reversed := false)
+# Used to animate or highlight the character speaking when it changes. Called by Dialgoic signal speaker_changed.
+func speaker_changed(new_speaker):
+	var new_character :DialogicCharacter = new_speaker["character"]
+	if not current_char or new_character != current_char: 
+		current_char = new_speaker["character"]
+		portrait_system.animate_character(new_character, "Tada", .2,)
+		get_namebox_side(new_character)
+	
+	
+	#TODO: extend the scripts for scientist and patient and have them call this function instead.
+func get_namebox_side(new_character):
+	var portraits_info = portrait_system.get_character_info(new_character)
+	if not portraits_info["joined"]: return
+	var char_position:String = portraits_info["position_id"]
+	var namebox_side = 0 if char_position.contains("left") else 0.5 if char_position.contains("center") else 1
+	var layout : DialogicLayoutBase = Dialogic.get_subsystem("Styles").get_layout_node()
+	var textbox = layout.get_node("VN_TextboxLayer")
+	if not textbox: return
+	var name_label: PanelContainer = textbox.get_node("%NameLabelPanel")
+	name_label.anchor_left = namebox_side
+	name_label.anchor_right = namebox_side
