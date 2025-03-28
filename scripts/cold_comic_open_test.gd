2c8522576a39fcfa559ba67d7b2c1a9603327c9d
TorqueMcFearson <160625369+TorqueMcFearson@users.noreply.github.com>
2025-03-28 14:11:23 -0700

    Comic Frame Test Tool
     - Just made a quick script and scene for testing out tweens inside a comic panel.

---
+++ scripts/cold_comic_open_test.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+const FADE_DURATION = 1.25
+const NEXT_SCENE = preload("res://scenes/ExplorationAreas/patientArea1.tscn")
+@onready var default_position := global_position
+@onready var button_container: MarginContainer = $"Button Container"
+@onready var canvas_modulate: CanvasModulate = $"../CanvasModulate"
+var current_tween:= Tween.new() 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#Dialogic.start("res://Dialogic_Items/Timelines/coldOpenScene.dtl")
+	Dialogic.signal_event.connect(DialogicSignal)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func DialogicSignal(arg: String):
+	if arg == "timeline_ended":
+		current_tween = create_tween()
+	current_tween.tween_property(canvas_modulate,"color",Color.BLACK,FADE_DURATION)
+	current_tween.tween_callback(func(): get_tree().change_scene_to_packed(NEXT_SCENE))
+		
+		
+
+
+func run_animations(extra_arg_0: String) -> void:
+	pass # Replace with function body.
